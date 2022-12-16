502a3b3457a56f686c2fae0103bb898f885aaf27
Ihor_Poplavskyi <poplawskyiihor@gmail.com>
2022-12-16 22:40:00 +0200

    done with logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import list from './data/guessItemsList';
 
@@ -9,37 +9,53 @@ interface IClicked {
 }
 
 const App: React.FC = () => {
+  const [state, setState] = useState(list);
   const [clicked, setClicked] = useState<IClicked[]>([]);
   const [timers, setTimers] = useState<ReturnType<typeof setTimeout>[]>([]);
-  const myRefs = useRef<HTMLButtonElement[]>([]);
-  console.log(clicked)
-  const clickHandler = (clickedItem: IClicked, index: number) => {
+
+  const clickHandler = (clickedItem: IClicked) => {
     setClicked(prev => [...prev, clickedItem])
-    myRefs.current[index].disabled = true;
+    setState(prev => [...prev.map((el) => {
+      if (el.id === clickedItem.id) {
+        el.disabled = true;
+      }
+      return el;
+    })])
     let timeout = setTimeout(() => {
-      myRefs.current[index].disabled = false;
-      setClicked([]);
-    }, 1500)
+      setState(prev => [...prev.map((el) => {
+        if (el.id === clickedItem.id) {
+          el.disabled = false;
+        }
+        return el;
+      })])
+      setClicked([])
+    }, 1000)
     setTimers(prev => [...prev, timeout])
   }
   useEffect(() => {
     if (clicked.length === 2 && clicked[0].id !== clicked[1].id && clicked[0].name === clicked[1].name) {
       setClicked([]);
-      timers.map(el => clearTimeout(el))
-    } else if (clicked.length === 2 && clicked[0].id === clicked[1].id) {
-      setClicked([]);
+      setState(prevState => prevState.map(el => {
+        if (el.id === clicked[0].id || el.id === clicked[1].id) {
+          el.disabled = true;
+        }
+        return el; 
+      }))
+      clearTimeout(timers.at(-1))
+      clearTimeout(timers.at(-2))
     } else if (clicked.length === 2 && clicked[0].id !== clicked[1].id && clicked[0].name !== clicked[1].name) {
-      setClicked([]);
+      setClicked(prevState => prevState.splice(0,1))
+    } else if (clicked.length === 2 && clicked[0].id === clicked[1].id) {
+      setClicked(prevState => prevState.splice(0, 1));
     }
   }, [clicked])
 
   return (
     <div className='container'>
       <div className='App'>
-        {list.map((el, index) => {
+        {state.map((el) => {
           return (
-            <button key={el.id} ref={(el) => (myRefs.current[index] = el as HTMLButtonElement)}
-              onClick={() => clickHandler(el, index)} className="flip-card">
+            <button disabled={el.disabled} onClick={() => clickHandler(el)} className="flip-card" key={el.id} >
               <div className="flip-card-inner">
                 <div className="flip-card-front"></div>
                 <div className="flip-card-back">
